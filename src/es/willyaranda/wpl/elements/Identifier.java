294e0ebbe98e7b32c52b96d1f34fbacc34d4177f
Guillermo López <willyaranda@gmail.com>
2011-11-30 23:52:07 +0100

    Identificadores y números

---
+++ src/es/willyaranda/wpl/elements/Identifier.java
@@ -0,0 +1,19 @@
+package es.willyaranda.wpl.elements;
+
+public class Identifier {
+	private String name = "";
+	private String type = "";
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
---
+++ src/es/willyaranda/wpl/elements/Number.java
@@ -0,0 +1,14 @@
+package es.willyaranda.wpl.elements;
+
+public class Number {
+	private long value = 0;
+
+	public long getValue() {
+		return value;
+	}
+
+	public void setValue(long value) {
+		this.value = value;
+	}
+	
+}
