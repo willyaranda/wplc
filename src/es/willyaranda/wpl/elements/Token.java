04af2355060fcfc3b80485bd02c08cbf67bf8749
Guillermo López <willyaranda@gmail.com>
2011-12-01 13:58:59 +0100

    Clase Token
    Signed-off-by: Guillermo López <willyaranda@gmail.com>

---
+++ src/es/willyaranda/wpl/elements/Token.java
@@ -0,0 +1,19 @@
+package es.willyaranda.wpl.elements;
+
+public class Token {
+	/**
+	 * <code>type</code> es el tipo de elemento encontrado. Puede ser de tres tipos:
+	 * 		* <code>ID</code> que es un identificador
+	 * 		* <code>NB</code> que es un número
+	 * 		* <code>RW</code> que es una palabra reservada
+	 */
+	public String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
